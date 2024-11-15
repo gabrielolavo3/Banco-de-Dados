9a95e9baa143d8e4d43369877508e723babd7027
Gabriel S. Olavo <gabrielsantosolavo101@gmail.com>
2024-11-15 16:32:49 -0300

    novo arquivo

--- ControleDeTurma/scriptSql/scriptCriacaoSql.sql
+++ ControleDeTurma/scriptSql/scriptCriacaoSql.sql
@@ -1,35 +1,37 @@
 CREATE DATABASE IF NOT EXISTS ControleDeTurma;
 USE ControleDeTurma;
 
-create table Curso 
+create table Curso
 (
-    IdCurso int primary key auto_increment,
+	IdCurso int primary key auto_increment,
     Nome varchar(100) not null,
     CH_Total int not null
 );
 
 create table Turma
 (
-    IdTurma int auto_increment,
+	IdTurma int primary key auto_increment,
     Sala varchar(12),
     Turno varchar(15) not null,
     FK_Curso_idCurso int,
-    PRIMARY KEY(
-	idTurma, 
-        FK_Curso_idCurso
-    )
+    
+    -- Adicionando Chave Estrangeira
+    CONSTRAINT fkCurso FOREIGN KEY (FK_Curso_idCurso) 
+    REFERENCES Curso(IdCurso) 
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 );
 
 create table Disciplina
 (
-    IdDisciplina int auto_increment,
+	IdDisciplina int primary key auto_increment,
     Nome varchar(30) not null,
     CargaHoraria int
 );
 
 create table Professor
 (
-    IdProfessor int auto_increment,
+	IdProfessor int primary key auto_increment,
     Nome varchar(20) not null,
     Sobrenome varchar(30) not null,
     Titulacao varchar(25)
@@ -37,19 +39,27 @@ create table Professor
 
 create table Ministra
 (
-    FK_Turma_IdTurma int,
+	FK_Turma_IdTurma int,
     FK_Disciplina_IdDisciplina int,
     FK_Professor_IdProfessor int,
     PRIMARY KEY (
-	FK_Turma_IdTurma, 
-	FK_Disciplina_IdDisciplina, 
-	FK_Professor_IdProfessor
-    )
+		FK_Turma_IdTurma, FK_Disciplina_IdDisciplina, FK_Professor_IdProfessor
+    ),
+    
+    CONSTRAINT fkTurma FOREIGN KEY(FK_Turma_IdTurma) REFERENCES Turma(IdTurma)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+    CONSTRAINT fkDisciplina FOREIGN KEY(FK_Disciplina_IdDisciplina) REFERENCES Disciplina(IdDisciplina)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+    CONSTRAINT fkProfessor FOREIGN KEY(FK_Professor_IdProfessor) REFERENCES Professor(IdProfessor)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 create table Aluno
 (
-    Matricula varchar(25),
+	Matricula varchar(25) primary key,
     Nome varchar(20) not null,
     Sobrenome varchar(30) not null,
     CPF char(11) unique not null,
@@ -62,20 +72,20 @@ create table Aluno
     Cidade varchar(45),
     Estado char(3),
     FK_Turma_IdTurma int,
-    PRIMARY KEY(
-	Matricula, 
-        FK_Turma_IdTurma
-    )
+    
+    CONSTRAINT fkIdTurma FOREIGN KEY(FK_Turma_IdTurma) REFERENCES Turma(IdTurma)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 );
 
 create table Telefone
 (
-    IdTelefone int auto_increment,
+	IdTelefone int primary key auto_increment,
     DDD int not null,
     Numero int not null,
     FK_Aluno_Matricula varchar(25),
-    PRIMARY KEY(
-	IdTelefone, 
-	FK_Aluno_Matricula
-    )
+    
+    CONSTRAINT fkAluno FOREIGN KEY(FK_Aluno_Matricula) REFERENCES Aluno(Matricula)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
 );
